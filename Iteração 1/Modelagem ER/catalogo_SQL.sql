e964dd17c4a04a7b8477e2436c0ba0af6a248fe2
Eduardo Santi <eduardo.senacrs@gmail.com>
2014-09-05 17:03:42 -0300

    Modelagem Entidade Relacional - Catál. de Produtos

---
+++ Iteração 1/Modelagem ER/catalogo_SQL.sql
@@ -0,0 +1,22 @@
+﻿create table produtos(
+	idProduto serial primary key,
+	nome varchar(50) not null,
+	preco decimal(10,2) not null,
+	idCategoria integer not null,
+	constraint idCategoriaFK foreign key (idCategoria) references categorias(idCategoria)
+);
+
+create table categorias(
+	idCategoria serial primary key,
+	descricao varchar(50) not null
+);
+
+insert into produtos (nome, preco, idCategoria) values ('Bolo de Chocolate', 1.50, 1),
+						       ('Pão de Queijo', 2.50, 3),
+						       ('Coca-Cola', 2.00, 2),
+						       ('Água Mineral - Sem Gás', 2.20, 2),
+						       ('Bolo de Cenoura', 2.00, 1);
+
+insert into categorias (descricao) values ('bolos'),
+			      ('bebidas'),
+			      ('salgados');
