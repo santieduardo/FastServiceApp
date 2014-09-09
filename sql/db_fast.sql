a9a71897d942f29f77a64d9a882030cd4e763c92
Eduardo Santi <eduardo.senacrs@gmail.com>
2014-09-08 21:52:11 -0300

    População BD e Update modelo ER

--- sql/db_fast.sql
+++ sql/db_fast.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.7.1
+-- version 4.1.6
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 08-Set-2014 às 22:03
--- Versão do servidor: 5.6.20
--- PHP Version: 5.5.15
+-- Generation Time: 09-Set-2014 às 02:50
+-- Versão do servidor: 5.6.16
+-- PHP Version: 5.5.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,17 +27,19 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `categorias` (
-`idCategoria` int(10) NOT NULL,
-  `nome` varchar(60) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+  `idCategoria` int(10) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(60) NOT NULL,
+  PRIMARY KEY (`idCategoria`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 --
 -- Extraindo dados da tabela `categorias`
 --
 
 INSERT INTO `categorias` (`idCategoria`, `nome`) VALUES
-(1, 'Bebidas'),
-(2, 'Salgados');
+(1, 'Salgados'),
+(2, 'Doces'),
+(3, 'Bebidas');
 
 -- --------------------------------------------------------
 
@@ -46,48 +48,27 @@ INSERT INTO `categorias` (`idCategoria`, `nome`) VALUES
 --
 
 CREATE TABLE IF NOT EXISTS `produtos` (
-  `idProduto` int(15) NOT NULL,
+  `idProduto` int(10) NOT NULL AUTO_INCREMENT,
   `categoria` int(10) NOT NULL,
   `nome` varchar(45) NOT NULL,
   `preco` decimal(10,2) NOT NULL,
-  `status` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `status` tinyint(1) NOT NULL,
+  PRIMARY KEY (`idProduto`),
+  KEY `fk_produtos_categorias_idx` (`categoria`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
 
 --
 -- Extraindo dados da tabela `produtos`
 --
 
 INSERT INTO `produtos` (`idProduto`, `categoria`, `nome`, `preco`, `status`) VALUES
-(1, 1, 'Coca Cola', '5.12', 1),
-(2, 1, 'Ice Tea Leão Fuze Pêssego', '4.50', 1),
-(3, 1, 'Ice Tea Leão Fuze Limão', '4.50', 0),
-(4, 2, 'Empanado de Frango', '3.00', 1);
+(1, 1, 'Empada de Frango', '3.50', 1),
+(2, 1, 'Bolinho de Carne', '2.75', 0),
+(3, 2, 'Bombom Amor Carioca', '1.25', 1),
+(4, 2, 'Balas Sortidas', '0.15', 1),
+(5, 3, 'Coca-Cola 250ml', '2.00', 1),
+(6, 3, 'Suco Del Valle Pêssego', '2.80', 1);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `categorias`
---
-ALTER TABLE `categorias`
- ADD PRIMARY KEY (`idCategoria`);
-
---
--- Indexes for table `produtos`
---
-ALTER TABLE `produtos`
- ADD PRIMARY KEY (`idProduto`), ADD KEY `fk_produtos_categorias_idx` (`categoria`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `categorias`
---
-ALTER TABLE `categorias`
-MODIFY `idCategoria` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 --
 -- Constraints for dumped tables
 --
@@ -96,7 +77,7 @@ MODIFY `idCategoria` int(10) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 -- Limitadores para a tabela `produtos`
 --
 ALTER TABLE `produtos`
-ADD CONSTRAINT `fk_produtos_categorias` FOREIGN KEY (`categoria`) REFERENCES `categorias` (`idCategoria`) ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_produtos_categorias` FOREIGN KEY (`categoria`) REFERENCES `categorias` (`idCategoria`) ON UPDATE NO ACTION;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
