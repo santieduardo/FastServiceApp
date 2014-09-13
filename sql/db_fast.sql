c2f52c4b959d1c5d1c4512ba81ba5b28d38b2b41
Henrique Rieger <henrique.rieger@globo.com>
2014-09-13 14:27:56 -0300

    Tabela de Usuários

--- sql/db_fast.sql
+++ sql/db_fast.sql
@@ -1,54 +1,43 @@
--- phpMyAdmin SQL Dump
--- version 4.1.12
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Sep 09, 2014 at 10:00 PM
--- Server version: 5.6.16
--- PHP Version: 5.5.11
+/*
+Navicat MySQL Data Transfer
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+Source Server         : _localhost
+Source Server Version : 50616
+Source Host           : localhost:3306
+Source Database       : db_fast
 
+Target Server Type    : MYSQL
+Target Server Version : 50616
+File Encoding         : 65001
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+Date: 2014-09-13 14:26:50
+*/
 
---
--- Database: `db_fast`
---
+SET FOREIGN_KEY_CHECKS=0;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `categorias`
---
-
-CREATE TABLE IF NOT EXISTS `categorias` (
+-- ----------------------------
+-- Table structure for `categorias`
+-- ----------------------------
+DROP TABLE IF EXISTS `categorias`;
+CREATE TABLE `categorias` (
   `idCategoria` int(10) NOT NULL AUTO_INCREMENT,
   `nome` varchar(60) NOT NULL,
   `label` varchar(30) NOT NULL DEFAULT 'default',
   PRIMARY KEY (`idCategoria`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `categorias`
---
+-- ----------------------------
+-- Records of categorias
+-- ----------------------------
+INSERT INTO `categorias` VALUES ('1', 'Salgados', 'info');
+INSERT INTO `categorias` VALUES ('2', 'Doces', 'success');
+INSERT INTO `categorias` VALUES ('3', 'Bebidas', 'primary');
 
-INSERT INTO `categorias` (`idCategoria`, `nome`, `label`) VALUES
-(1, 'Salgados', 'info'),
-(2, 'Doces', 'success'),
-(3, 'Bebidas', 'primary');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `produtos`
---
-
-CREATE TABLE IF NOT EXISTS `produtos` (
+-- ----------------------------
+-- Table structure for `produtos`
+-- ----------------------------
+DROP TABLE IF EXISTS `produtos`;
+CREATE TABLE `produtos` (
   `idProduto` int(10) NOT NULL AUTO_INCREMENT,
   `categoria` int(10) NOT NULL,
   `nome` varchar(45) NOT NULL,
@@ -56,31 +45,34 @@ CREATE TABLE IF NOT EXISTS `produtos` (
   `status` tinyint(1) NOT NULL,
   `arquivo` varchar(45) NOT NULL DEFAULT 'default.jpg',
   PRIMARY KEY (`idProduto`),
-  KEY `fk_produtos_categorias_idx` (`categoria`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+  KEY `fk_produtos_categorias_idx` (`categoria`),
+  CONSTRAINT `fk_produtos_categorias` FOREIGN KEY (`categoria`) REFERENCES `categorias` (`idCategoria`) ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `produtos`
---
+-- ----------------------------
+-- Records of produtos
+-- ----------------------------
+INSERT INTO `produtos` VALUES ('1', '1', 'Empada de Frango', '3.50', '1', 'salgado.jpg');
+INSERT INTO `produtos` VALUES ('2', '1', 'Bolinho de Carne', '2.75', '0', 'default.jpg');
+INSERT INTO `produtos` VALUES ('3', '2', 'Bombom Amor Carioca', '1.25', '1', 'default.jpg');
+INSERT INTO `produtos` VALUES ('4', '2', 'Balas Sortidas', '0.15', '1', 'default.jpg');
+INSERT INTO `produtos` VALUES ('5', '3', 'Coca-Cola Lata', '2.00', '1', 'cocacola.jpg');
+INSERT INTO `produtos` VALUES ('6', '3', 'Suco Del Valle Pêssego', '2.80', '1', 'default.jpg');
 
-INSERT INTO `produtos` (`idProduto`, `categoria`, `nome`, `preco`, `status`, `arquivo`) VALUES
-(1, 1, 'Empada de Frango', '3.50', 1, 'salgado.jpg'),
-(2, 1, 'Bolinho de Carne', '2.75', 0, 'default.jpg'),
-(3, 2, 'Bombom Amor Carioca', '1.25', 1, 'default.jpg'),
-(4, 2, 'Balas Sortidas', '0.15', 1, 'default.jpg'),
-(5, 3, 'Coca-Cola Lata', '2.00', 1, 'cocacola.jpg'),
-(6, 3, 'Suco Del Valle Pêssego', '2.80', 1, 'default.jpg');
+-- ----------------------------
+-- Table structure for `usuarios`
+-- ----------------------------
+DROP TABLE IF EXISTS `usuarios`;
+CREATE TABLE `usuarios` (
+  `idUsuario` int(15) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(40) NOT NULL,
+  `sobrenome` varchar(40) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `senha` char(40) NOT NULL,
+  `ctime` int(16) NOT NULL,
+  PRIMARY KEY (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `produtos`
---
-ALTER TABLE `produtos`
-  ADD CONSTRAINT `fk_produtos_categorias` FOREIGN KEY (`categoria`) REFERENCES `categorias` (`idCategoria`) ON UPDATE NO ACTION;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- ----------------------------
+-- Records of usuarios
+-- ----------------------------
