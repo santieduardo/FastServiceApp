91da1d48a42352b268662232f86bf1dde692342d
Henrique Rieger <henrique.rieger@globo.com>
2014-09-30 20:25:45 -0300

    Composer e Facebook

--- src/index.php
+++ src/index.php
@@ -200,6 +200,9 @@ if (defined('ENVIRONMENT'))
  * And away we go...
  *
  */
+
+
+require_once __DIR__ . '/vendor/autoload.php';
 require_once BASEPATH.'core/CodeIgniter.php';
 
 /* End of file index.php */
