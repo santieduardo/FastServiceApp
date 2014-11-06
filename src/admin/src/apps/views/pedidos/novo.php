512bbae6201d5cdc454a428643c5bfc5de24e74c
jenniferbenedetti <jenniferbenedetti@yahoo.com.br>
2014-11-05 22:47:45 -0200

    Telas de consulta/novo pedido (admin)

--- src/admin/src/apps/views/pedidos/novo.php
+++ src/admin/src/apps/views/pedidos/novo.php
@@ -1,76 +1,167 @@
-<h4 class="page-header">Dados Bancários - <small>Cadastrar</small></h4>
-<div class="row hidden-print">
-	<div class="col-md-12">
-		<div class="btn-group">
-			<a href="<?=site_url('contas'); ?>" class="btn btn-default">
-				 <span class="glyphicon glyphicon-chevron-left"></span> Voltar
-			</a>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+ 
+	<title>Novo Pedido</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="imagetoolbar" content="no">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+<link rel="apple-touch-icon-precomposed" href="http://localhost/FastServiceApp/src/admin/src/favicon.png">
+<link rel="icon" href="http://localhost/FastServiceApp/src/admin/src/favicon.ico">
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="google" content="notranslate">
+<meta name="robots" content="noindex, nofollow">
+
+<link rel="stylesheet" type="text/css" href="C:/xampp/htdocs/FastServiceApp/src/admin/src/assets/css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="C:/xampp/htdocs/FastServiceApp/src/admin/src/assets/css/main.css">
+
+<script type="text/javascript" src="http://localhost/FastServiceApp/src/admin/src/assets/js/jquery-1.11.1.js"></script>
+<script type="text/javascript" src="http://localhost/FastServiceApp/src/admin/src/assets/js/bootstrap.js"></script>
+<script type="text/javascript" src="http://localhost/FastServiceApp/src/admin/src/assets/js/plugins/maskedinput.js"></script>
+<script type="text/javascript" src="http://localhost/FastServiceApp/src/admin/src/assets/js/plugins/validate.js"></script>
+<script type="text/javascript" src="http://localhost/FastServiceApp/src/admin/src/assets/js/main.js"></script>
+
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+</head>
+<body>
+<nav class="navbar navbar-default navbar navbar-fixed-top hidden-print" role="navigation">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+		</div>
+		<div class="collapse navbar-collapse">
+			<ul class="nav navbar-nav">
+				<!-- Novo -->
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Pedido <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="<a href="http://localhost/FastServiceApp/src/admin/src/index.php/descontos">Fazer Pedido</a></li>
+						<li><a href="<a href="http://localhost/FastServiceApp/src/admin/src/index.php/descontos">Consultar Pedido</a></li>
+						
+					</ul>
+				</li>		
+				<li><a href="http://localhost/FastServiceApp/src/admin/src/index.php/logoff">Sair</a></li>	
+			</ul>
+			<p class="navbar-text navbar-right hidden-xs hidden-sm">Logado como Eduardo Santi</p>
 		</div>
 	</div>
-</div>
-<br>
-
-<form action="<?=site_url('contas/novo'); ?>" method="post" id="contas-form">
+</nav>
+<div class="container-fluid">
 	<div class="row">
-		<div class="col-md-12 col-md-offset-0">
+	<div class="col-md-3">
+		<div class="col-sm-3 col-md-2 sidebar hidden-print">
 			<div class="row">
-				<div class="col-md-6 form-group">
-					<label>Cliente / Fornecedor *</label>
-					<select name="id_cliente" class="form-control" required="required">
-						<option value="">Selecione um cliente</option>
-						<?php
-							foreach($clientes as $cliente){
-								if(post('id_cliente') == $cliente->id_cliente){
-									echo '<option value="',$cliente->id_cliente,'" selected>',$cliente->nome,'</option>';
-								} else {
-									echo '<option value="',$cliente->id_cliente,'">',$cliente->nome,'</option>';
-	
-								}
-							}
-						?>
-					</select>
-				</div>
-				
-				<div class="col-md-6 form-group">
-					<label>Favorecido</label>
-					<input type="text" maxlength="250" required="required" class="form-control" name="favorecido" value="<?=post('favorecido'); ?>">
-				</div>
-			</div>
-			
-			<div class="row">
-				<div class="col-md-3 form-group">
-					<label>Banco *</label>
-					<input type="text" maxlength="150" required="required" class="form-control" name="banco" value="<?=post('banco'); ?>">
-				</div>
-				
-				<div class="col-md-3 form-group">
-					<label>Agência *</label>
-					<input type="text" maxlength="50" required="required" class="form-control" name="agencia" value="<?=post('agencia'); ?>">
-				</div>
-
-				<div class="col-md-3 form-group">
-					<label>Conta *</label>
-					<input type="text" maxlength="75" required="required" class="form-control" name="cc" value="<?=post('cc'); ?>">
-				</div>
-				
-				<div class="col-md-3 form-group">
-					<label>CNPJ/CPF *</label>
-					<input type="text" maxlength="150" required="required" class="form-control" name="cnpj" value="<?=post('cnpj'); ?>">
-				</div>
-			</div>
-			
-			<div class="row hidden-print">
-				<div class="col-md-4 col-md-offset-8">
-					<button type="submit" class="btn btn-success pull-right">
-						 <span class="glyphicon glyphicon-ok"></span> Cadastrar
-					</button>
-				</div>
-			</div>
-		</div>
+	<div class="col-md-12 text-center">
+		<a href="<?=site_url('views/pedidos/novo'); ?>" class="thumbnail">
+			<span class="glyphicon glyphicon-plus-sign"></span>
+			<br>
+			Fazer Pedido
+		</a>
 	</div>
-</form>
+</div>	
 
-<script type="text/javascript">
-	$("#contas-form").validate();
+<div class="row">
+	<div class="col-md-12 text-center">
+		<a href="<?=site_url('views/pedidos/lista'); ?>" class="thumbnail">
+			<span class="glyphicon glyphicon-refresh"></span>
+			<br>
+			Consultar Pedido
+		</a>
+	</div>
+</div>	
+
+
+<div class="row">
+	<div class="col-md-12 text-center">
+		<a href="<?=site_url('logoff'); ?>" class="thumbnail">
+			<span class="glyphicon glyphicon-log-out"></span>
+			<br>
+			Sair
+		</a>
+	</div>
+</div>	
+		</div>
 	
-</script>
+			
+<div class="dashboard">
+<div class="col-md-9">
+ <h1 style="text-align: center">Novo Pedido
+ </h1>
+ 
+  <div class="table-responsive">
+      <table class="table">
+       <thead>
+       
+	      <tr>
+		     <th>
+               <form role="form">
+                <div class="form-group">
+                 <input type="search" class="form-control" placeholder="Digite o nome do produto">
+                </div>
+               </form>
+             </th>
+       
+         
+            <th>
+              <a href="#" class="btn btn-info btn-lg">
+                <span class="glyphicon glyphicon-search"> </span>
+              </a>
+           </th>
+         </tr>  
+       </thead>
+      </table>
+      
+     <table class="table">
+     
+       <thead>
+       
+	      <tr>
+		     <th> Produto </th>
+		     <th>Valor</th>				
+	      </tr>
+	  	</thead>	
+	  		
+	    <tbody>				
+		  <tr>			
+			 <th> Produto 1 </th>
+			 <th> 05,30</th>			 
+			 <th> <a href="#" class="btn btn-info btn-lg">
+                   <span class="glyphicon glyphicon-ok"> </span>
+                  </a>
+             </th>					
+		  </tr>		  
+		   <tr>			
+			 <th> Produto 2 </th>
+			 <th> 02,50</th>			 
+			 <th> <a href="#" class="btn btn-info btn-lg">
+                   <span class="glyphicon glyphicon-ok"> </span>
+                  </a>
+             </th>					
+		  </tr>
+	</tbody>
+  </table>	
+  <h1>
+  
+<button type="reset" class="btn btn-default">Cancelar</button>
+
+
+<button type="submit" class="btn btn-info btn-lg">Concluir</button>
+</div>							
+</h1>
+
+
+
+   
+ </body>
+</html>
