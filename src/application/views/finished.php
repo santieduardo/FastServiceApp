12b88fd43021bc333f1561ad98340f1793971cfa
hschwabg <schwabhenrique@gmail.com>
2014-11-23 15:16:00 -0200

    Cancelamento local do último pedido
    Fazer funcionar tela de confirmação de cancelamento.

--- src/application/views/finished.php
+++ src/application/views/finished.php
@@ -36,10 +36,10 @@
 							
 							<div class="well well-sm">
 								Total: <span class="pull-right" id="total">R$ <?=reais($pedido->total); ?></span>
+							</div>
 							<a href="<?=site_url('checkout/cancelar/' . $pedido->idPedido); ?>" class="btn btn-default">
 								<span class="glyphicon glyphicon-trash"></span>
 							</a>
-							</div>
 					
 							<div class="text-center">
 								<img alt="" src="<?=site_url('checkout/qr/'.$pedido->idPedido); ?>" width="400">
